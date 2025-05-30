4b2ba5464dbe910fc2f2bd024759635ec4db7627
DungHM <dunghm@rikkeisoft.com>
2025-05-30 17:01:14 +0700

    init

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var trueAns = 0
+    @State var threeCountries = ["Estonia",
+                                 "France",
+                                 "Germany"]
+    @State var point = 0
+    let countries = [
+        "Estonia",
+        "France",
+        "Germany",
+        "Ireland",
+        "Italy",
+        "Monaco",
+        "Nigeria",
+        "Poland",
+        "Spain",
+        "UK",
+        "Ukraine",
+        "US"
+    ]
+    
+    func random3countries() -> [String] {
+        return Array(countries.shuffled().prefix(3))
+    }
+    
+    func randomTrueAns() -> Int {
+        return Int.random(in: 0...2)
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            VStack {
+                Text("\(threeCountries[trueAns])")
+                    .bold()
+                ForEach(0...2, id: \.self) { idx in
+                    VStack {
+                        Image("\(threeCountries[idx])")
+                        Text("\(threeCountries[idx])")
+                    }
+                        .onTapGesture {
+                            if idx == trueAns {
+                                point += 1
+                            }
+                            threeCountries = random3countries()
+                            trueAns = randomTrueAns()
+                        }
+                }
+                Text("Point: \(point)")
+            }
+
         }
-        .padding()
     }
 }
 
