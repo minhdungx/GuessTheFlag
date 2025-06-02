3bc91b049872174b6d2ca6a83c22dd36a1bb1e3a
DungHM <dunghm@rikkeisoft.com>
2025-06-02 10:11:13 +0700

    UI

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -36,28 +36,69 @@ struct ContentView: View {
     func randomTrueAns() -> Int {
         return Int.random(in: 0...2)
     }
-
+    
     var body: some View {
+        
+        
         GeometryReader { geo in
-            VStack {
-                Text("\(threeCountries[trueAns])")
-                    .bold()
-                ForEach(0...2, id: \.self) { idx in
-                    VStack {
-                        Image("\(threeCountries[idx])")
-                        Text("\(threeCountries[idx])")
-                    }
-                        .onTapGesture {
-                            if idx == trueAns {
-                                point += 1
-                            }
-                            threeCountries = random3countries()
-                            trueAns = randomTrueAns()
+            ZStack() {
+                
+                RadialGradient(stops: [.init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                                       .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+                ], center: .top, startRadius: 200, endRadius: 700)
+                    .ignoresSafeArea()
+                
+                
+                
+                VStack {
+                    Spacer()
+                    Text("Guess the flag")
+                        .font(.largeTitle.weight(.semibold))
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                    VStack(alignment: .center, spacing: 15) {
+                        
+                        VStack {
+                            Text("Tap the flag of")
+                                .fontWeight(.bold)
+                                .foregroundStyle(.secondary)
+                                .padding(.top, 10)
+                            Text("\(threeCountries[trueAns])")
+                                .bold()
+                                .font(.system(size: 30))
+                            
                         }
+                        ForEach(0...2, id: \.self) { idx in
+                            Image("\(threeCountries[idx])")
+                                .renderingMode(.original)
+                                .clipShape(.capsule)
+                                .shadow(radius: 5
+                                )
+                                .onTapGesture {
+                                    if idx == trueAns {
+                                        point += 1
+                                    }
+                                    threeCountries = random3countries()
+                                    trueAns = randomTrueAns()
+                                }
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(20)
+                    .background(.thinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    Spacer()
+                    Spacer()
+                    
+                    Text("Score: \(point)")
+                        .font(.largeTitle.weight(.bold))
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                    Spacer()
                 }
-                Text("Point: \(point)")
+                .padding()
+                
             }
-
         }
     }
 }
